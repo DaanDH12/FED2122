daa5834284e23eb7d839281c07c88f4b9403b71f
Daan <49617687+DaanDH12@users.noreply.github.com>
2021-12-01 13:42:18 +0100

    Finished first page
    Missing:
    
    - Menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
 // JavaScript Document
+
+
+// elementen opgehaald die ik wil animeren 
+const allAnimationItems = document.querySelectorAll("section")
+
+const options = {
+    rootMargin: "20px",
+    threshold: 0.10
+}
+
+function callbackFunction(entries) {
+    entries.forEach(entry => {
+        // check of het element in beeld is
+        if (entry.intersectionRatio > 0) {
+          //voeg de class toe wanneer deze in beeld is
+            entry.target.classList.add('fade')   
+        }
+        else {
+            entry.target.classList.remove('fade')  
+        }
+    })
+}
+
+const observer = new IntersectionObserver(callbackFunction, options)
+
+allAnimationItems.forEach(item => {
+    //observeer het element
+    observer.observe(item)
+})
